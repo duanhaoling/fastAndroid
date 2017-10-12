927d3cec678e01c8cf394e218bf1475fd0b67c80
ldh <lingduanhao@gamil.com>
2017-10-12 15:09:21 +0800

    android7.0文件URL离开应用适配

---
+++ src/main/java/com/ldh/androidlib/adaptive/FileProvider7.java
@@ -0,0 +1,65 @@
+package com.ldh.androidlib.adaptive;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
+
+import java.io.File;
+
+/**
+ * Created by ldh on 2017/10/12.
+ * <p>
+ * http://blog.csdn.net/lmj623565791/article/details/72859156
+ * 对于面向 Android 7.0 的应用，Android 框架执行的 StrictMode API 政策禁止在您的应用外部公开 file:// URI。
+ * 如果一项包含文件 URI 的 intent 离开您的应用，则应用出现故障，并出现 FileUriExposedException 异常。
+ */
+
+public class FileProvider7 {
+
+    public static Uri getUriForFile(Context context, File file) {
+        Uri fileUri = null;
+        if (Build.VERSION.SDK_INT >= 24) {
+            fileUri = getUriForFile24(context, file);
+        } else {
+            fileUri = Uri.fromFile(file);
+        }
+        return fileUri;
+    }
+
+    private static Uri getUriForFile24(Context context, File file) {
+        Uri fileUri = FileProvider.getUriForFile(context,
+                context.getPackageName() + ".android7.fileprovider",
+                file);
+        return fileUri;
+    }
+
+    /**
+     * 例如：安装apk调用
+     * FileProvider7.setIntentDataAndType(this,
+     * intent, "application/vnd.android.package-archive", file, true);
+     *
+     * @param context
+     * @param intent
+     * @param type
+     * @param file
+     * @param writeAble
+     */
+    public static void setIntentDataAndType(Context context,
+                                            Intent intent,
+                                            String type,
+                                            File file,
+                                            boolean writeAble) {
+        if (Build.VERSION.SDK_INT >= 24) {
+            intent.setDataAndType(getUriForFile(context, file), type);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            if (writeAble) {
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            }
+        } else {
+            intent.setDataAndType(Uri.fromFile(file), type);
+        }
+    }
+
+}
